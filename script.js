33f327d4d32cb3a7061c85f5a9d711f542c4d87e
Ghozy165 <85822663+Ghozy165@users.noreply.github.com>
2025-01-31 16:08:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const rateButton = document.querySelector('.rate');
+    const submitButton = document.querySelector('.btn-submit');
+    const rateScore = document.getElementById('score');
+    const ratingCard = document.querySelector('.rating');
+    const thanksCard = document.querySelector('.thanks');
+  
+    let selectedRate = null;
+  
+    if (!rateButton || !submitButton || !rateScore || !ratingCard || !thanksCard) {
+      console.error('Error: Element tidak ditemukan.');
+      return;
+    }
+  
+    rateButton.addEventListener('click', (event) => {
+      if (event.target.classList.contains('btn-rate')) {
+        selectedRate = event.target.textContent;
+      }
+    });
+  
+    submitButton.addEventListener('click', () => {
+      if (!selectedRate) {
+        alert('Please select a rating first.');
+        return;
+      }
+  
+      ratingCard.classList.remove('isOn');
+      rateScore.textContent = selectedRate;
+      thanksCard.classList.add('isOn');
+    });
+  });
+  
